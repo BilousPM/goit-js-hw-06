ee41ccebfa078e3f6f9abe63a04ba676c1579fc8
Petro Bilous <bilouspm@gmail.com>
2023-02-22 17:16:18 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -14,6 +14,30 @@ const images = [
 ];
 
 
+
+const listGaleryEl = document.querySelector('.gallery');
+
+// listGaleryEl.insertAdjacentHTML('beforeend', `<li><img src="" alt=""></li>`);
+// console.log(listGaleryEl)
+
+
+
+
+const markup = images.map(image => {
+  console.log(image.url)
+  listGaleryEl.insertAdjacentHTML('beforeend', `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+})
+
+
+// const imageGaleryEl = document.createElement('img');
+// imageGaleryEl.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+// imageGaleryEl.alt = 'Group of Horses Running'
+// galleryItemEl.append(imageGaleryEl);
+// console.log(imageGaleryEl.src);
+
+// listGaleryEl.append(galleryItemEl)
+
+
 // Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
 
 // <ul class="gallery"></ul>
