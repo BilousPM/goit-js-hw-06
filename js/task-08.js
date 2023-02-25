c5d47351bd7e576e52565fd21d02dac156c78636
Petro Bilous <bilouspm@gmail.com>
2023-02-25 02:47:39 +0200

    performs task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,93 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', handleFormSubmit)
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Усі поля мають бути заповнені');
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value
+  };
+  console.log(formData);
+ 
+
+  event.target.reset();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     event.target.reset()
+    
+//     // console.log('ghghghgg')
+// })
+
+
+
+
+
+
+// const inputEmailEl = document.querySelector(`input[type="email"]`)
+// const inputPasswordEl = document.querySelector(`input[type="password"]`)
+// const buttonSubmitEl = document.querySelector(`button[type="submit"]`)
+
+// console.log(inputEmailEl.value)
+
+
+
+// inputEmailEl.addEventListener('input', (event) => {
+//   return event.target.value
+// })
+
+
+// inputPasswordEl.addEventListener('input', (event) => {
+//     console.log(event.target.value)
+// })
+
+// formEl.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     event.target.reset()
+    
+//     // console.log('ghghghgg')
+// })
+
+// console.log(inputEmailEl.value)
+// formEl.addEventListener('submit', handleCheckFieldsComplete);
+
+// function handleCheckFieldsComplete(event) {
+
+//     console.log(inputEmailEl.);
+//     // if (inputEmailEl.value === '' || inputPasswordEl.value === "") {
+//     //     alert('mmfgmgg')
+//     // }
+// }
